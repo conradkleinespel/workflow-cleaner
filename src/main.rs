a3eef4c31b060935522732b9e9123a99ee5487a1
Conrad Kleinespel <conradk@conradk.com>
2024-10-27 13:44:13 +0100

    Delete infinite number of workflow runs

--- src/main.rs
+++ src/main.rs
@@ -33,16 +33,11 @@ fn main() {
 
     let repos = fetch_repos(&client, &headers);
 
-    let default_pagination_size = 30;
     let cutoff_date = Utc::now() - Duration::days(7);
 
     for repo in repos {
         println!("Looking at {}", repo);
-        while delete_workflow_runs(client.clone(), headers.clone(), cutoff_date, repo.as_str())
-            == default_pagination_size
-        {
-            continue;
-        }
+        delete_workflow_runs(client.clone(), headers.clone(), cutoff_date, repo.as_str());
     }
 }
 
@@ -62,40 +57,43 @@ fn delete_workflow_runs(
     headers: HeaderMap,
     cutoff_date: DateTime<Utc>,
     repo: &str,
-) -> usize {
+) {
     let url = format!("https://api.github.com/repos/{}/actions/runs", repo);
-    let response = client.get(&url).headers(headers.clone()).send();
 
-    if response.is_err() {
-        return 0;
-    }
+    let mut deleted_workflow_runs_num = 1;
 
-    let json_object = response.unwrap().json::<WorkflowRunsResponse>();
-    if json_object.is_err() {
-        return 0;
-    }
+    while deleted_workflow_runs_num > 0 {
+        let response = client.get(&url).headers(headers.clone()).send();
+        if response.is_err() {
+            return;
+        }
 
-    let mut num_deleted_runs = 0;
-    for run in json_object.unwrap().workflow_runs {
-        let run_date = run.created_at.parse::<DateTime<Utc>>().unwrap();
-        if run_date < cutoff_date {
-            let delete_url = format!(
-                "https://api.github.com/repos/{}/actions/runs/{}",
-                repo, run.id
-            );
-            let response = client.delete(&delete_url).headers(headers.clone()).send();
-            if response.is_ok() {
-                num_deleted_runs += 1;
-                println!(
-                    "Deleted run {} from repo {}, response text = \"{}\"",
-                    run.id,
-                    repo,
-                    response.unwrap().text().unwrap()
+        let json_object = response.unwrap().json::<WorkflowRunsResponse>();
+        if json_object.is_err() {
+            return;
+        }
+
+        deleted_workflow_runs_num = 0;
+        for run in json_object.unwrap().workflow_runs {
+            let run_date = run.created_at.parse::<DateTime<Utc>>().unwrap();
+            if run_date < cutoff_date {
+                let delete_url = format!(
+                    "https://api.github.com/repos/{}/actions/runs/{}",
+                    repo, run.id
                 );
-            } else {
-                println!("Deletion failed: {:?}", response.err().unwrap())
+                let response = client.delete(&delete_url).headers(headers.clone()).send();
+                if response.is_ok() {
+                    deleted_workflow_runs_num += 1;
+                    println!(
+                        "Deleted run {} from repo {}, response text = \"{}\"",
+                        run.id,
+                        repo,
+                        response.unwrap().text().unwrap()
+                    );
+                } else {
+                    println!("Deletion failed: {:?}", response.err().unwrap())
+                }
             }
         }
     }
-    return num_deleted_runs;
 }
